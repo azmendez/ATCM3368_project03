5315b5031ddc8fd2a5c5995deb4222907169b727
Ariana <ariana.z.mendez@gmail.com>
2021-11-13 02:47:16 -0600

    Added rudimentary pick up system

---
+++ Assets/Scripts/PickUpItems.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItems : MonoBehaviour
+{
+    [SerializeField] Transform _objectDestination;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            PickUpItem();
+        }
+        else
+        {
+            DropItem();
+        }
+    }
+
+    void PickUpItem()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = _objectDestination.position;
+        this.transform.parent = GameObject.Find("ObjectDestination").transform;
+    }
+
+    void DropItem()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+
+    }
+
+}
